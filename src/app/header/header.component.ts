b27cceee420b169daa8e60955851a3e237306a84
emnarbii <emna.rbii@esprit.tn>
2025-02-11 09:05:41 +0100

    data_binding

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  link1:string='Home';
+  link2:string="Residences";
+  url:string="https://esprit.tn/";
+  linkColor:string=" "
+
+// changeColor(){
+// this.linkColor= this.linkColor;
+// }
+  clickMe(){
+    alert(" vous avez cliquer!!!!!");
+  }
+}
---
+++ src/app/residences/residences.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Residence } from '../core/models/Residence';
+
+@Component({
+  selector: 'app-residences',
+  templateUrl: './residences.component.html',
+  styleUrls: ['./residences.component.css'],
+})
+export class ResidencesComponent {
+  address:string="";
+  listResidences: Residence[] = [
+    {
+      id: 1,
+      name: 'El fel',
+      address: 'Borj Cedria',
+      image: '../../assets/images/R1.jpg',
+      status: 'Disponible',
+    },
+    {
+      id: 2,
+      name: 'El yasmine',
+      address: 'Ezzahra',
+      image: '../../assets/images/R2.jpg',
+      status: 'Disponible',
+    },
+    {
+      id: 3,
+      name: 'El Arij',
+      address: 'Rades',
+      image: '../../assets/images/R1.jpg',
+      status: 'Vendu',
+    },
+    {
+      id: 4,
+      name: 'El Anber',
+      address: 'inconnu',
+      image: '../../assets/images/R2.jpg',
+      status: 'En Construction',
+    },
+  ];
+
+  showLocation(address:string) {
+    if (address === 'inconnu') {
+      return alert("l'adresse est inconnue");
+    } else {
+     return alert("l'adresse est  "+address);
+    }
+  }
+}
